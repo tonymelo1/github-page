4ab80efe5a8acf904307ecbc09c7310ab36cbc71
Tony Melo <ton.mel1996@gmail.com>
2020-08-10 04:32:54 -0400

    Updates

---
+++ precache-manifest.2bd5683f8a46e5f29f20db14612c5301.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4716252256d1a97016b6b8b1b0b851df",
+    "url": "/github-page/index.html"
+  },
+  {
+    "revision": "c93352392a2b35819fac",
+    "url": "/github-page/static/css/main.106290d7.chunk.css"
+  },
+  {
+    "revision": "2e2245bd004dec4ee578",
+    "url": "/github-page/static/js/2.453c2352.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/github-page/static/js/2.453c2352.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c93352392a2b35819fac",
+    "url": "/github-page/static/js/main.590d1e4d.chunk.js"
+  },
+  {
+    "revision": "9b55972bb59066d8fb32",
+    "url": "/github-page/static/js/runtime-main.d6abdcb6.js"
+  }
+]);
